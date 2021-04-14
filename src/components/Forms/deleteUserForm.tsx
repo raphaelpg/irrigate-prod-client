5e5b7f4c55776ce567cfa6f51fb217b33850bb3f
raphaelpg <raphael_pinto@outlook.com>
2021-04-14 14:24:17 +0100

    add delete account confirmation pop up

--- src/components/Forms/deleteUserForm.tsx
+++ src/components/Forms/deleteUserForm.tsx
@@ -5,6 +5,11 @@ import { IUser } from '../../interfaces/User';
 
 const DeleteUserForm: React.FC = () => {
   const [status, setStatus] = useState<string>('');
+  const [displayConfirmation, setDisplayConfirmation] = useState<boolean>(false);
+
+  const toggleConfirmation = () => {
+    displayConfirmation ? setDisplayConfirmation(false) : setDisplayConfirmation(true);
+  }
 
   const deleteUser = () => {
     if (localStorage.getItem('user')) {
@@ -13,6 +18,7 @@ const DeleteUserForm: React.FC = () => {
       .then(res => res.json())
       .then((result) => {
         if (result.msg === 'User deleted') {
+          toggleConfirmation();
           setStatus('SUCCESS');
           AuthService.logout();
           setTimeout(() => {
@@ -29,8 +35,24 @@ const DeleteUserForm: React.FC = () => {
   return(
     <div className="delete-container">
       <h2 className="delete-label">Delete user</h2>
-      {status === "SUCCESS" ? <p className="form-result">User account delete, see you soon</p> : <button onClick={deleteUser} className="deleteFormButton" type="submit">Delete account</button>}
+      {status === "SUCCESS" ? <p className="form-result">User account deleted, see you soon</p> : <button onClick={toggleConfirmation} className="deleteFormButton" type="submit">Delete account</button>}
       {status === "ERROR" && <p className="form-result-error">There was an error, please try later.</p>}
+      {displayConfirmation 
+        ? (
+          <div>
+            <div className="confirmation-form">
+              <p style={{margin: "auto"}}>Please confirm deletion of your account:</p>
+              <div className="confirmation-button-container">
+                <button className="cancel-button" onClick={toggleConfirmation}>Cancel</button>
+                <button className="deleteFormButton" onClick={deleteUser}>Confirm</button>
+              </div>
+            </div>
+          </div>
+        ) 
+        : (
+          <>
+          </>
+        )}
     </div>
   );
 };
